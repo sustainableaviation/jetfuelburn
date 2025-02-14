01a959db855b04afcbfd2fcce5bd143c75f4b0a6
Michael Weinold <23102087+michaelweinold@users.noreply.github.com>
2025-02-14 11:10:32 -0500

    updated docstrings

--- jetfuelburn/diagrams.py
+++ jetfuelburn/diagrams.py
@@ -8,43 +8,22 @@ def calculate_fuel_consumption_based_on_payload_range(
     d: float,
     oew: float,
     mtow: float,
-    payload_point_A: float,
-    range_point_A: float,
     payload_point_B: float,
     range_point_B: float,
+    payload_point_C: float,
     range_point_C: float,
+    range_point_D: float,
 ) -> tuple[ureg.Quantity, ureg.Quantity]:
-    """
-    Given aircraft performance parameters from a payload/range diagram and a flight distance,
-    calculates the fuel burned and payload mass for a given mission.
+    r"""
+    Given aircraft performance parameters from a payload/range diagram and a flight distance $d$,
+    calculates the required fuel mass $m_F$ and payload mass $m_{PL}$ for a given mission.
 
-    ![Screenshot](docs/_static/payload_range_generic_DOCSTRINGS.svg)
+    This function implements the simple _"reading fuel mass off the payload/range diagram directly"_ method:
 
-    Parameters
-    ----------
-    d : float
-        Flight distance [length]
-    oew : float
-        Operating Empty Weight (OEW) of the aircraft [mass]
-    mtow : float
-        Maximum Takeoff Weight (MTOW) of the aircraft [mass]
-    payload_point_A : float
-        Payload mass at point A [mass]
-    range_point_A : float
-        Range at point A [length]
-    payload_point_B : float
-        Payload mass at point B [mass]
-    range_point_B : float
-        Range at point B [length]
-    range_point_C : float
-        Range at point C [length]
-
-    Warnings
-    --------
-    There is no manufacturer-independent convention for including
-    reserve and alternate fuel in payload/range diagrams.
-    Limited evidence suggests that eg. Boeing always assumes a 200nmi diversion distance.
-    The user is therefore cautioned against directly comparing aircraft of different manufacturers.
+    ![Payload/Range Diagram](https://raw.githubusercontent.com/sustainableaviation/jetfuelburn/refs/heads/main/docs/_static/payload_range_generic_DOCSTRINGS.svg)
+    Payload/Range diagram of the Airbus A350-900 (data derived [from manufacturer information](https://web.archive.org/web/20211129144142/https://www.airbus.com/sites/g/files/jlcbta136/files/2021-11/Airbus-Commercial-Aircraft-AC-A350-900-1000.pdf)).
+    Note that in this figure, the y-axis shows _total_ aircraft weight, not _payload weight_.
+    Total aircraft weight can be computed by adding the operating empty weight (OEW) to the payload weight and fuel weight.
 
     Notes
     -----
@@ -54,21 +33,50 @@ def calculate_fuel_consumption_based_on_payload_range(
     |-------------------|---------------------------------------------------------------------|
     | data availability | excellent for existing aircraft (payload/range diagrams are public) |
     | aircraft payload  | maximum payload possible payload                                    |
+    | climb/descent     | implicitly considered                                               |
     | fuel reserves     | depends on (sometimes unknown) manufacturer assumptions             |
+    | alternate airport | depends on (sometimes unknown) manufacturer assumptions             |
     
-
     See Also
     --------
     - [Burzlaff (2017), Fig. 3.10](https://www.fzt.haw-hamburg.de/pers/Scholz/arbeiten/TextBurzlaff.pdf)
     - ["Aircraft Payload-Range Analysis for Financiers", Fig. 5 ff.](http://www.aircraftmonitor.com/uploads/1/5/9/9/15993320/aircraft_payload_range_analysis_for_financiers___v2.pdf)
 
+    Warnings
+    --------
+    There is no manufacturer-independent convention for including
+    reserve and alternate fuel in payload/range diagrams.
+    Limited evidence suggests that eg. Boeing always assumes a 200nmi diversion distance (cf. [Slide 43 in this Boeing presentation](https://web.archive.org/web/20250000000000*/http://aviation.itu.edu.tr//img/aviation/datafiles/Lecture%20Notes/FundamentalsofAirlineManagement20152016/Lecture%20Notes/10.1-ITU_Airplane_Performance.pdf) and [this airliners.net discussion](https://www.airliners.net/forum/viewtopic.php?t=764183#p11029989)).
+    The user is therefore cautioned against directly comparing aircraft of different manufacturers using this function.
+
+    Parameters
+    ----------
+    d : float
+        Flight distance [length]
+    oew : float
+        Operating Empty Weight (OEW) of the aircraft [mass]
+    mtow : float
+        Maximum Takeoff Weight (MTOW) of the aircraft [mass]
+    payload_point_B : float
+        Payload mass at point A [mass]
+    range_point_B : float
+        Range at point A [length]
+    payload_point_C : float
+        Payload mass at point B [mass]
+    range_point_C : float
+        Range at point B [length]
+    range_point_D : float
+        Range at point C [length]
+
     Returns
     -------
     tuple[ureg.Quantity, ureg.Quantity]
-        Fuel burned during flight [mass], Payload mass [mass]
+        Required fuel [mass], Payload mass [mass]
 
     Raises
     ------
+    ValueError
+        If any of the input parameters are not [pint](https://pint.readthedocs.io/en/stable/) Quantity instances of the correct dimension.
     ValueError
         If the distance is less than zero.
     ValueError
@@ -78,32 +86,32 @@ def calculate_fuel_consumption_based_on_payload_range(
     #  all variables of the same dimension must use consistent units
     oew = oew.to('kg')
     mtow = mtow.to('kg')
-    payload_point_A = payload_point_A.to('kg')
     payload_point_B = payload_point_B.to('kg')
-    range_point_A = range_point_A.to('km')
+    payload_point_C = payload_point_C.to('kg')
     range_point_B = range_point_B.to('km')
     range_point_C = range_point_C.to('km')
+    range_point_D = range_point_D.to('km')
 
-    acft_weight_point_A = oew + payload_point_A
-    acft_weight_point_B = oew + payload_point_B
+    acft_weight_point_A = oew + payload_point_B
+    acft_weight_point_B = oew + payload_point_C
     acft_weight_point_C = oew
 
     if d < 0:
         raise ValueError("Distance must be greater than zero.")
-    if d < range_point_A:
-        slope = (mtow - acft_weight_point_A)/range_point_A
+    if d < range_point_B:
+        slope = (mtow - acft_weight_point_A)/range_point_B
         intercept = acft_weight_point_A
         m_f = (slope * d + intercept) - acft_weight_point_A
-        m_pld = payload_point_A
-    elif range_point_A <= d < range_point_B:
-        slope = (acft_weight_point_B - acft_weight_point_A) / (range_point_B - range_point_A)
-        intercept = range_point_A * -slope + acft_weight_point_A
+        m_pld = payload_point_B
+    elif range_point_B <= d < range_point_C:
+        slope = (acft_weight_point_B - acft_weight_point_A) / (range_point_C - range_point_B)
+        intercept = range_point_B * -slope + acft_weight_point_A
         m_f = mtow - (slope * d + intercept)
         m_pld = (slope * d + intercept) - oew
-    elif range_point_B <= d < range_point_C:
+    elif range_point_C <= d < range_point_D:
         m_f = mtow - acft_weight_point_B # max fuel
-        slope = (acft_weight_point_C - acft_weight_point_B) / (range_point_C - range_point_B)
-        intercept = range_point_B * -slope + acft_weight_point_B
+        slope = (acft_weight_point_C - acft_weight_point_B) / (range_point_D - range_point_C)
+        intercept = range_point_C * -slope + acft_weight_point_B
         m_pld = (slope * d + intercept) - oew
     else:
         raise ValueError("Distance exceeds maximum range of aircraft as per payload-range diagram.")
