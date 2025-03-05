96d798e4a43a3cd6d22f98fbff33feeab0800553
Michael Weinold <23102087+michaelweinold@users.noreply.github.com>
2025-03-05 13:24:43 -0700

    added test

--- tests/fixtures/reducedorder.py
+++ tests/fixtures/reducedorder.py
@@ -6,6 +6,7 @@ ureg = pint.get_application_registry() # https://pint-pandas.readthedocs.io/en/l
 
 @pytest.fixture
 def fixture_yanto_etal_B739_heavy():
+    # Panel (b) of Figure 5 in Yanto et al. (2017)
     input_data = {
         'acft':'B739',
         'R':4724*ureg.km,
@@ -17,10 +18,61 @@ def fixture_yanto_etal_B739_heavy():
 
 @pytest.fixture
 def fixture_yanto_etal_B739_light():
+    # Panel (b) of Figure 5 in Yanto et al. (2017)
     input_data = {
         'acft':'B739',
         'R':2943*ureg.km,
         'PL':7885*ureg.kg,
     }
     output_data = 8878*ureg.kg
+    return input_data, output_data
+
+
+@pytest.fixture
+def fixture_aim2015_777_light_10k():
+    # Figure 4 in Dray et al. (2019) https://doi.org/10.1016/j.tranpol.2019.04.013
+    input_data = {
+        'acft_size_class':8,
+        'D_climb':300*ureg.km,
+        'D_cruise':(10000-300-200)*ureg.km,
+        'D_descent':200*ureg.km,
+        'PL':4.5*ureg.metric_ton
+    }
+    output_data = 72000*ureg.kg
+    return input_data, output_data
+
+
+@pytest.fixture
+def fixture_aim2015_777_light_15k():
+    # Figure 4 in Dray et al. (2019) https://doi.org/10.1016/j.tranpol.2019.04.013
+    input_data = {
+        'acft_size_class':8,
+        'D_climb':300*ureg.km,
+        'D_cruise':(15000-300-200)*ureg.km,
+        'D_descent':200*ureg.km,
+        'PL':4.5*ureg.metric_ton
+    }
+    output_data = 121843*ureg.kg
+    return input_data, output_data
+
+
+@pytest.fixture
+def fixture_seymour_a321_5500km():
+    # Figure 1 in Reduced_Model_Shape_Exploration.ipynb
+    input_data = {
+        'acft':'B738',
+        'R':5557*ureg.km,
+    }
+    output_data = 16728*ureg.kg
+    return input_data, output_data
+
+
+@pytest.fixture
+def fixture_seymour_a321_1000km():
+    # Figure 1 in Reduced_Model_Shape_Exploration.ipynb
+    input_data = {
+        'acft':'B738',
+        'R':902*ureg.km,
+    }
+    output_data = 4008*ureg.kg
     return input_data, output_data
--- tests/test_reducedorder.py
+++ tests/test_reducedorder.py
@@ -14,10 +14,16 @@ from jetfuelburn.aux.tests import approx_with_units
 from .fixtures.reducedorder import (
     fixture_yanto_etal_B739_heavy,
     fixture_yanto_etal_B739_light,
+    fixture_aim2015_777_light_10k,
+    fixture_aim2015_777_light_15k,
+    fixture_seymour_a321_5500km,
+    fixture_seymour_a321_1000km,
 )
 
 from jetfuelburn.reducedorder import (
     yanto_etal,
+    aim2015,
+    seymour_etal,
 )
 
 
@@ -25,7 +31,7 @@ from jetfuelburn.reducedorder import (
     "fixture_name",
     ["fixture_yanto_etal_B739_heavy", "fixture_yanto_etal_B739_light"]
 )
-def test_calculate_atmospheric_conditions(request, fixture_name):
+def test_yanto_etal(request, fixture_name):
     fixture = request.getfixturevalue(fixture_name)
     input_data, output_data = fixture
 
@@ -35,6 +41,47 @@ def test_calculate_atmospheric_conditions(request, fixture_name):
         PL=input_data['PL']
     )
 
+    assert approx_with_units(
+        value_check=weight_fuel,
+        value_expected=output_data,
+        rel=0.05
+    )
+
+
+@pytest.mark.parametrize(
+    "fixture_name",
+    ["fixture_aim2015_777_light_10k", "fixture_aim2015_777_light_15k"]
+)
+def test_aim2015(request, fixture_name):
+    fixture = request.getfixturevalue(fixture_name)
+    input_data, output_data = fixture
+
+    weight_fuel = aim2015(
+        acft_size_class=input_data['acft_size_class'],
+        D_climb=input_data['D_climb'],
+        D_cruise=input_data['D_cruise'],
+        D_descent=input_data['D_descent'],
+        PL=input_data['PL']
+    )
+    assert approx_with_units(
+        value_check=weight_fuel,
+        value_expected=output_data,
+        rel=0.075
+    )
+
+
+@pytest.mark.parametrize(
+    "fixture_name",
+    ["fixture_seymour_a321_1000km", "fixture_seymour_a321_5500km"]
+)
+def test_seymour_etal(request, fixture_name):
+    fixture = request.getfixturevalue(fixture_name)
+    input_data, output_data = fixture
+
+    weight_fuel = seymour_etal.calculate_fuel(
+        acft=input_data['acft'],
+        R=input_data['R']
+    )
     assert approx_with_units(
         value_check=weight_fuel,
         value_expected=output_data,
