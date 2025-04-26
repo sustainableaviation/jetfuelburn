08fab229baea237ce010f8e57a0e7b09e2fc2f6e
Michael Weinold <23102087+michaelweinold@users.noreply.github.com>
2025-04-26 11:50:07 -0600

    updated functions

---
+++ jetfuelburn/data/USDOT/2023/__init__.py
@@ -0,0 +1,10 @@
+"""
+# Files
+
+## US Department of Transportation (DOT)
+
+### `L_AIRCRAFT_TYPE.csv`
+
+- Source: https://www.transtats.bts.gov/Fields.asp?gnoyr_VQ=FIH
+- Description: Air Carrier Summary: T2: U.S. Air Carrier TRAFFIC And Capacity Statistics by Aircraft Type, Field: Aircraft Type
+"""
